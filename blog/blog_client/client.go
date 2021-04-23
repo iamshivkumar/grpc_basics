8d9c74ed5d463576d298dfe2792a239836e7c4af
Shivkumar Konade <shivkonade123@gmail.com>
2021-04-23 09:18:28 +0530

    create & read

--- blog/blog_client/client.go
+++ blog/blog_client/client.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
-	// "github.com/shivkumar123g/grpc_go_course/blog/blogpb"
+
+	"github.com/shivkumar123g/grpc_go_course/blog/blogpb"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 )
@@ -20,6 +23,31 @@ func main() {
 		log.Fatalf("could not connect: %v", err)
 	}
 	defer cc.Close()
-	// c := blogpb.BlogServiceClient(cc)
+	c := blogpb.NewBlogServiceClient(cc)
+	readBlog(c)
 
+}
+
+func createBlog(c blogpb.BlogServiceClient) {
+	res, err := c.CreateBlog(context.Background(), &blogpb.CreateBlogRequest{
+		Blog: &blogpb.Blog{
+			AutherId: "1",
+			Title:    "Title 3",
+			Content:  "Content 3",
+		},
+	})
+	if err != nil {
+		log.Fatalf("Unexpected error: %v", err)
+	}
+	fmt.Println(res)
+}
+
+func readBlog(c blogpb.BlogServiceClient) {
+	res, err := c.ReadBlog(context.Background(), &blogpb.ReadBlogRequest{
+		BlogId: "608236bcf26ded5be685f4a8",//608236bcf26ded5be685f4a8
+	})
+	if err != nil {
+		fmt.Printf("ERROR: can't read: %v",err)
+	}
+	fmt.Println(res)
 }
--- blog/blog_server/server.go
+++ blog/blog_server/server.go
@@ -10,26 +10,86 @@ import (
 
 	"github.com/joho/godotenv"
 	"github.com/shivkumar123g/grpc_go_course/blog/blogpb"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/status"
 )
 
-var collection *mongo.Collection 
+var collection *mongo.Collection
 
 type server struct{}
 
-type blogItem  struct {
-	ID primitive.ObjectID `bson:"_id,omitempty"`
-	AuthorID string `bson:"author_id"`
-	Content string `bson:"content"`
-	Title string `bson:"title"`
+type blogItem struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
+	AuthorID string             `bson:"author_id"`
+	Content  string             `bson:"content"`
+	Title    string             `bson:"title"`
+}
+
+func (*server) CreateBlog(ctx context.Context, req *blogpb.CreateBlogRequest) (*blogpb.CreateBlogResponse, error) {
+	blog := req.GetBlog()
+	data := blogItem{
+		AuthorID: blog.GetAutherId(),
+		Content:  blog.GetContent(),
+		Title:    blog.GetTitle(),
+	}
+	res, err := collection.InsertOne(context.Background(), data)
+	if err != nil {
+		return nil, status.Errorf(
+			codes.Internal,
+			fmt.Sprintf("Internal error: %v", err),
+		)
+	}
+	oid, ok := res.InsertedID.(primitive.ObjectID)
+	if !ok {
+		return nil, status.Errorf(
+			codes.Internal,
+			fmt.Sprint("Cannot convert to OID"),
+		)
+	}
+	return &blogpb.CreateBlogResponse{
+		Blog: &blogpb.Blog{
+			Id:       oid.Hex(),
+			AutherId: blog.GetAutherId(),
+			Title:    blog.GetTitle(),
+			Content:  blog.GetContent(),
+		},
+	}, nil
+}
+
+func (*server) ReadBlog(ctx context.Context, req *blogpb.ReadBlogRequest) (*blogpb.ReadBlogResponse, error) {
+	blogID := req.GetBlogId()
+	oid, err := primitive.ObjectIDFromHex(blogID)
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, fmt.Sprint("Cannot parse ID"))
+	}
+	data := &blogItem{}
+	filter := bson.D{primitive.E{Key: "_id", Value: oid}}
+	res := collection.FindOne(context.Background(), filter)
+	if err := res.Decode(data); err!=nil {
+      return nil, status.Errorf(
+		  codes.NotFound,
+		  fmt.Sprintf("Cannot find blog with specified ID: %v",err),
+	  )
+	}
+	return &blogpb.ReadBlogResponse{
+		Blog: &blogpb.Blog{
+			Id: data.ID.Hex(),
+			AutherId: data.AuthorID,
+			Title: data.Title,
+			Content: data.Content,
+		},
+	}, nil
+
 }
 
 func main() {
-    //if we crash the go code, we get the file name and line number
+	//if we crash the go code, we get the file name and line number
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
 	//connect to mongodb
@@ -51,7 +111,7 @@ func main() {
 	// 	log.Fatal(err)
 	// }
 
-    collection = client.Database("mydb").Collection("blog")
+	collection = client.Database("mydb").Collection("blog")
 
 	lis, err := net.Listen("tcp", "0.0.0.0:50051")
 	if err != nil {
